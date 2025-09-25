842039689fddd8844a85e80bdfa0e5925b165a0c
kumar <krishnaraogandupalli2000@gmail.com>
2025-09-26 01:58:06 +0530

    add code working

--- client/src/components/AuthPage.jsx
+++ client/src/components/AuthPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import {
   User,
   Mail,
@@ -9,6 +10,7 @@ import {
   LogIn,
   Shield,
 } from "lucide-react";
+import logo from "../../logo.png";
 
 const AuthPage = ({ onLogin }) => {
   const [isLogin, setIsLogin] = useState(true);
@@ -94,11 +96,17 @@ const AuthPage = ({ onLogin }) => {
       <div className="max-w-md w-full bg-white rounded-3xl shadow-2xl overflow-hidden">
         {/* Header */}
         <div className="bg-gradient-to-r from-cyan-500 to-blue-600 text-white p-8 text-center">
-          <div className="w-20 h-20 bg-white bg-opacity-20 rounded-full flex items-center justify-center mx-auto mb-4">
+          <div className="w-50 h-50 rounded-full flex items-center justify-center mx-auto mb-4 overflow-hidden">
+            <img 
+    src={logo} 
+    alt="CitizenConnect Logo" 
+    className="w-full h-full object-cover"
+  />
+
             <Shield className="w-10 h-10" />
           </div>
-          <h1 className="text-2xl font-bold mb-2">CitizenConnect</h1>
-          <p className="text-cyan-100 text-sm">Your Voice, Our Priority</p>
+    
+          <p className="text-cyan-100 text-sm" style={{fontWeight:700,fontSize:"40px"}}>Bridging The Gap</p>
         </div>
 
         {/* Form */}
@@ -153,6 +161,27 @@ const AuthPage = ({ onLogin }) => {
                 )}
               </div>
             )}
+             <div>
+                <label className="block text-sm font-medium text-gray-700 mb-2">
+                  Aadhaar Number
+                </label>
+                <div className="relative">
+                  <User className="absolute left-3 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400" />
+                  <input
+                    type="text"
+                    value={formData.number}
+                    onChange={(e) => handleInputChange("number", e.target.value)}
+                    className={`w-full pl-12 pr-4 py-3 border rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all ${
+                      errors.name ? "border-red-500" : "border-gray-300"
+                    }`}
+                    placeholder="Enter your Aadhaar number"
+                    autoComplete="number"
+                  />
+                </div>
+                {errors.number && (
+                  <p className="text-red-500 text-sm mt-1">{errors.number}</p>
+                )}
+              </div>
 
             <div>
               <label className="block text-sm font-medium text-gray-700 mb-2">
